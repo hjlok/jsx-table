6b945ba05f5549129eca145fb83cad5ac966db5c
hjl <296022012@qq.com>
2022-09-12 22:01:51 +0800

    实现编译前复制文件到指定目录

---
+++ src/components/test.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <div>{{ msg }}</div>
+        <el-button>试试</el-button>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            msg: 'hello'
+        }
+    }
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,52 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <test />
   </div>
 </template>
 
 <script>
+import test from '@/components/test'
+
 export default {
-  name: 'HomeView'
+  name: 'HomeView',
+  components: {
+    test
+  },
+  // created() {
+  //   const files = require.context('@/components', true, /\.vue$/)
+  //   files.keys().forEach(name => {
+  //     if (name=== './test.vue') {
+  //       const file = files(name)
+  //       console.log('hello wenj jiann', file)
+  //     }
+  //   })
+  // },
+  mounted() {
+    this.readTestFile()
+  },
+  methods: {
+    // 读取test.properties
+    readTestFile() {
+      const file = this.loadFile('./vue/test.vue')
+      console.info(file)
+      // console.log(this.unicodeToUtf8(file))
+    },
+    // 读取文件
+    loadFile(name) {
+        const xhr = new XMLHttpRequest()
+        const okStatus = document.location.protocol === 'file:' ? 0 : 200
+        xhr.open('GET', name, false)
+        xhr.overrideMimeType('text/html;charset=utf-8')
+        // 默认为utf-8
+        xhr.send(null)
+        return xhr.status === okStatus ? xhr.responseText : null
+    },
+    // unicode转utf-8
+    // unicodeToUtf8(data) {
+    //   data = data.replace(/\\/g, '%')
+    //   return unescape(data)
+    // }
+  }
 }
 </script>
